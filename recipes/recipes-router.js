ee4d7dc49e76826f1b29e44d9a59162d24b7d3b7
Barbara Moore <barbaraloismoore@gmail.com>
2020-07-29 02:07:35 -0400

    further work on ingredients

--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -39,10 +39,12 @@ router.get('/:id/ingredients', (req, res) => {
   const { id } = req.params;
 
   Ingredients.findByRecipe(id)
-    .then((ingredient) => {
-      console.log('ingredient:', ingredient);
+    .then((ingredients) => {
+      res.status(200).json({ data: ingredients });
     })
-    .catch((err) => console.log(err));
+    .catch((err) =>
+      res.status(500).json({ message: 'unable to retrieve ingredients' })
+    );
 });
 
 // GET - A recipe's instructions by ID
