3dfdf554473ba1a158fcc376488d4c0d387c7d1c
Barbara Moore <barbaraloismoore@gmail.com>
2020-07-29 01:37:33 -0400

    fixing dotenv issue

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const pgConnection = process.env.DATABASE_URL;
 
 module.exports = {
--- users/users-router.js
+++ users/users-router.js
@@ -3,6 +3,7 @@ const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const router = require('express').Router();
 const Users = require('./users-model.js');
+require('dotenv').config();
 
 router.use(express.json());
 
